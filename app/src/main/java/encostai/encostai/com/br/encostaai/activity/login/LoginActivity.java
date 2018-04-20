7ff2871226cc40cb23106333604928a524125ff4
MatheusGregorio <matheusvga1810@gmail.com>
2018-04-20 16:51:56 -0300

    Cadastro,Login, Mapa

--- app/src/main/java/encostai/encostai/com/br/encostaai/activity/login/LoginActivity.java
+++ app/src/main/java/encostai/encostai/com/br/encostaai/activity/login/LoginActivity.java
@@ -1,16 +1,41 @@
 package encostai.encostai.com.br.encostaai.activity.login;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import encostai.encostai.com.br.encostaai.R;
+import encostai.encostai.com.br.encostaai.activity.cadastro.CadastroActivity;
+import encostai.encostai.com.br.encostaai.activity.inicial.InicialActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity implements View.OnClickListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent;
+        switch (v.getId()) {
+
+            case R.id.entrar:
+                intent = new Intent(LoginActivity.this, InicialActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.cadastrar:
+                intent = new Intent(LoginActivity.this, CadastroActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/encostai/encostai/com/br/encostaai/activity/politicaTermos/PoliticaTermosActivity.java
@@ -0,0 +1,23 @@
+package encostai.encostai.com.br.encostaai.activity.politicaTermos;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import encostai.encostai.com.br.encostaai.R;
+
+public class PoliticaTermosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_politica_termos);
+
+
+
+    }
+
+}
