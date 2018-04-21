6ee9bb1770933b248d55e47239de0e70f117bf2c
Vitor Poncell <vitorponcell@hotmail.com>
2018-04-21 02:17:25 -0300

    Implementado calsse Permissions

--- app/src/main/java/encostai/encostai/com/br/encostaai/activity/login/LoginActivity.java
+++ app/src/main/java/encostai/encostai/com/br/encostaai/activity/login/LoginActivity.java
@@ -3,7 +3,10 @@ package encostai.encostai.com.br.encostaai.activity.login;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+
 import encostai.encostai.com.br.encostaai.R;
+import encostai.encostai.com.br.encostaai.utils.FirebaseConfig;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,4 +16,5 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
     }
+
 }
